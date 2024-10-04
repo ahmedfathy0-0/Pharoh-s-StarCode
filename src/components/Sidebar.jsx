f7c361cef9c52446e6426aa46f27cbae9cc9bfbe
Ammar Yasser <143096817+AMYasserF@users.noreply.github.com>
2024-10-04 21:26:29 +0300

    sidebar moons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = ({ isOpen, toggleSidebar, onPlanetClick }) => {
   const toggleMoons = (planet) => {
     setExpandedMoons((prevState) => ({
       ...prevState,
-      [planet]: !prevState[planet] // Toggle the state for the clicked planet
+      [planet]: !prevState[planet], // Toggle the state for the clicked planet
     }));
   };
 
@@ -17,31 +17,62 @@ const Sidebar = ({ isOpen, toggleSidebar, onPlanetClick }) => {
     <div className={`sidebar ${isOpen ? 'open' : ''}`}>
       <h2>NAVIGATION</h2>
       <ul className="planets">
-      <li className='Mercury' onClick={() => onPlanetClick(1)}><a href="#">Mercury</a></li>
-      <li className='Venus' onClick={() => onPlanetClick(2)}><a href="#">Venus</a></li>
-      <li className='Earth' onClick={() => onPlanetClick(3)}>
-          <a href="#">Earth</a>
-          <ul>
+        <li className='Mercury' onClick={() => onPlanetClick(1)}>
+          <a href="#">Mercury</a>
+        </li>
+        <li className='Venus' onClick={() => onPlanetClick(2)}>
+          <a href="#">Venus</a>
+        </li>
+        <li className='Earth' onClick={() => onPlanetClick(3)}>
+          <a href="#" >Earth</a>
+          <span className='moon-icon' onClick={(e) => { e.stopPropagation(); toggleMoons('Earth'); }}>
+            🌕 {/* You can replace this with an SVG or a Font Icon */}
+          </span>
+    
+            <ul className={expandedMoons['Earth']? 'show' : ''}>
               <li className='Moon' onClick={(e) => { e.stopPropagation(); onPlanetClick(13); }}>
-                  <a href="#">Moon</a>
+                <a href="#">Moon</a>
               </li>
-          </ul>
-      </li>
-        <li className='Mars' onClick={() => onPlanetClick(4)}>
-          <a href="#">Mars</a>           
-           <ul>
-            <li className='Phobos' onClick={(e) => { e.stopPropagation(); onPlanetClick(14); }}>
-              <a href='#'>Phobos</a>
-            </li>
-          </ul>
-        </li>
-        <li className='Jupiter' onClick={() => onPlanetClick(5)}><a href="#">Jupiter</a>            <ul>
-            <li className='Europa' onClick={(e) => { e.stopPropagation(); onPlanetClick(15); }}><a href='#'>Europa</a></li>
             </ul>
+          
         </li>
-        <li className='Saturn' onClick={() => onPlanetClick(6)}><a href="#">Saturn</a>            <ul>
-            <li className='Titan' onClick={(e) => { e.stopPropagation(); onPlanetClick(16); }}><a href='#'>Titan</a></li>
+        <li className='Mars'  onClick={() => onPlanetClick(4)}>
+          <a href="#">Mars</a>
+          <span className='moon-icon' onClick={(e) => { e.stopPropagation(); toggleMoons('Mars'); }}>
+            🌕 {/* You can replace this with an SVG or a Font Icon */}
+          </span>
+            <ul className={expandedMoons['Mars']? 'show' : ''}>
+              <li className='Phobos' onClick={(e) => { e.stopPropagation(); onPlanetClick(14); }}>
+                <a href='#'>Phobos</a>
+              </li>
             </ul>
+          
+        </li>
+        <li className='Jupiter' onClick={() => onPlanetClick(5)}>
+          <a href="#" >Jupiter</a>
+          <span className='moon-icon' onClick={(e) => { e.stopPropagation(); toggleMoons('Jupiter'); }}>
+            🌕 {/* You can replace this with an SVG or a Font Icon */}
+          </span>
+          
+            <ul className={expandedMoons['Jupiter']? 'show' : ''}>
+              <li className='Europa' onClick={(e) => { e.stopPropagation(); onPlanetClick(15); }}>
+                <a href='#'>Europa</a>
+              </li>
+            </ul>
+          
+        </li>
+        <li className='Saturn'  onClick={() => onPlanetClick(6)}>
+          <a href="#">Saturn</a>
+          <span className='moon-icon' onClick={(e) => { e.stopPropagation(); toggleMoons('Saturn'); }}>
+            🌕 {/* You can replace this with an SVG or a Font Icon */}
+          </span>
+          
+            <ul className={expandedMoons['Saturn']? 'show' : ''}>
+              <li className='Titan' onClick={(e) => { e.stopPropagation(); onPlanetClick(16); }}>
+                <a href='#'>Titan</a>
+              </li>
+            </ul>
+        
         </li>
         <li className='Uranus' onClick={() => onPlanetClick(7)}>
           <a href="#">Uranus</a>
