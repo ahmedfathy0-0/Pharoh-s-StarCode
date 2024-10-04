3716f57d1a5ea2b14f8c98b76ffbf2785cf4af2d
ahmedfathy0-0 <ahmedfathi20044002@gmail.com>
2024-10-04 23:38:35 +0300

    add some styles to sidemenu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import './Sidebar.css';
+import { FiArrowRight } from "react-icons/fi";
+import mercuryImage from '../images/mercury.png';
+import venusImage from '../images/venus.png';
+import earthImage from '../images/earth.png';
+import moonImage from '../images/moon.png';
+import marsImage from '../images/mars.png';
+import phobosImage from '../images/phobos.png';
+import jupiterImage from '../images/jupiter.png';
+import europaImage from '../images/europa.png';
+import saturnImage from '../images/saturn.png';
+import titanImage from '../images/titan.png';
+import uranusImage from '../images/uranus.png';
+import neptuneImage from '../images/neptune.png';
 
 const Sidebar = ({ isOpen, toggleSidebar, onPlanetClick }) => {
-  // State to track open/close status of moons list for each planet
-  const [expandedMoons, setExpandedMoons] = useState({});
+  const [expandedMoons, setExpandedMoons] = useState([false, false, false, false, false, false, false, false]);
 
-  // Toggle moons visibility for the clicked planet
-  const toggleMoons = (planet) => {
-    setExpandedMoons((prevState) => ({
-      ...prevState,
-      [planet]: !prevState[planet], // Toggle the state for the clicked planet
+  const toggleMoons = (id) => {
+    setExpandedMoons(() => ({
+      [0]: false,
+      [1]: false,
+      [2]: false,
+      [3]: false,
+      [4]: false,
+      [5]: false,
+      [6]: false,
+      [id]: true,
     }));
   };
 
@@ -18,71 +35,80 @@ const Sidebar = ({ isOpen, toggleSidebar, onPlanetClick }) => {
       <h2>NAVIGATION</h2>
       <ul className="planets">
         <li className='Mercury' onClick={() => onPlanetClick(1)}>
+          <img src={mercuryImage} alt="Mercury" />
           <a href="#">Mercury</a>
         </li>
         <li className='Venus' onClick={() => onPlanetClick(2)}>
+          <img src={venusImage} alt="Venus" />
           <a href="#">Venus</a>
         </li>
         <li className='Earth' onClick={() => onPlanetClick(3)}>
-          <a href="#" >Earth</a>
-          <span className='moon-icon' onClick={(e) => { e.stopPropagation(); toggleMoons('Earth'); }}>
-            🌕 {/* You can replace this with an SVG or a Font Icon */}
-          </span>
-    
-            <ul className={expandedMoons['Earth']? 'show' : ''}>
-              <li className='Moon' onClick={(e) => { e.stopPropagation(); onPlanetClick(13); }}>
-                <a href="#">Moon</a>
-              </li>
-            </ul>
-          
+          <img src={earthImage} alt="Earth" />
+          <a href="#">Earth 
+            {!expandedMoons[3] && (
+              <FiArrowRight className="moon-icon" onClick={(e) => { e.stopPropagation(); toggleMoons(3); }} />
+            )}
+          </a>
+          <ul className={expandedMoons[3] ? 'show' : ''}>
+            <li className='Moon' onClick={(e) => { e.stopPropagation(); onPlanetClick(13); }}>
+              <img src={moonImage} alt="Moon" />
+              <a href="#">Moon</a>
+            </li>
+          </ul>
         </li>
-        <li className='Mars'  onClick={() => onPlanetClick(4)}>
-          <a href="#">Mars</a>
-          <span className='moon-icon' onClick={(e) => { e.stopPropagation(); toggleMoons('Mars'); }}>
-            🌕 {/* You can replace this with an SVG or a Font Icon */}
-          </span>
-            <ul className={expandedMoons['Mars']? 'show' : ''}>
-              <li className='Phobos' onClick={(e) => { e.stopPropagation(); onPlanetClick(14); }}>
-                <a href='#'>Phobos</a>
-              </li>
-            </ul>
-          
+        <li className='Mars' onClick={() => onPlanetClick(4)}>
+          <img src={marsImage} alt="Mars" />
+          <a href="#">Mars 
+            {!expandedMoons[4] && (
+              <FiArrowRight className="moon-icon" onClick={(e) => { e.stopPropagation(); toggleMoons(4); }} />
+            )}
+          </a>
+          <ul className={expandedMoons[4] ? 'show' : ''}>
+            <li className='Phobos' onClick={(e) => { e.stopPropagation(); onPlanetClick(14); }}>
+              <img src={phobosImage} alt="Phobos" />
+              <a href='#'>Phobos</a>
+            </li>
+          </ul>
         </li>
         <li className='Jupiter' onClick={() => onPlanetClick(5)}>
-          <a href="#" >Jupiter</a>
-          <span className='moon-icon' onClick={(e) => { e.stopPropagation(); toggleMoons('Jupiter'); }}>
-            🌕 {/* You can replace this with an SVG or a Font Icon */}
-          </span>
-          
-            <ul className={expandedMoons['Jupiter']? 'show' : ''}>
-              <li className='Europa' onClick={(e) => { e.stopPropagation(); onPlanetClick(15); }}>
-                <a href='#'>Europa</a>
-              </li>
-            </ul>
-          
+          <img src={jupiterImage} alt="Jupiter" />
+          <a href="#">Jupiter 
+            {!expandedMoons[5] && (
+              <FiArrowRight className="moon-icon" onClick={(e) => { e.stopPropagation(); toggleMoons(5); }} />
+            )}
+          </a>
+          <ul className={expandedMoons[5] ? 'show' : ''}>
+            <li className='Europa' onClick={(e) => { e.stopPropagation(); onPlanetClick(15); }}>
+              <img src={europaImage} alt="Europa" />
+              <a href='#'>Europa</a>
+            </li>
+          </ul>
         </li>
-        <li className='Saturn'  onClick={() => onPlanetClick(6)}>
-          <a href="#">Saturn</a>
-          <span className='moon-icon' onClick={(e) => { e.stopPropagation(); toggleMoons('Saturn'); }}>
-            🌕 {/* You can replace this with an SVG or a Font Icon */}
-          </span>
-          
-            <ul className={expandedMoons['Saturn']? 'show' : ''}>
-              <li className='Titan' onClick={(e) => { e.stopPropagation(); onPlanetClick(16); }}>
-                <a href='#'>Titan</a>
-              </li>
-            </ul>
-        
+        <li className='Saturn' onClick={() => onPlanetClick(6)}>
+          <img src={saturnImage} alt="Saturn" />
+          <a href="#">Saturn  
+            {!expandedMoons[6] && (
+              <FiArrowRight className="moon-icon" onClick={(e) => { e.stopPropagation(); toggleMoons(6); }} />
+            )}
+          </a>
+          <ul className={expandedMoons[6] ? 'show' : ''}>
+            <li className='Titan' onClick={(e) => { e.stopPropagation(); onPlanetClick(16); }}>
+              <img src={titanImage} alt="Titan" />
+              <a href='#'>Titan</a>
+            </li>
+          </ul>
         </li>
         <li className='Uranus' onClick={() => onPlanetClick(7)}>
+          <img src={uranusImage} alt="Uranus" />
           <a href="#">Uranus</a>
         </li>
         <li className='Neptune' onClick={() => onPlanetClick(8)}>
+          <img src={neptuneImage} alt="Neptune" />
           <a href="#">Neptune</a>
         </li>
       </ul>
 
-      <div className="sidebar-button" onClick={toggleSidebar}>
+      <div className="sidebar-button" onClick={() => { toggleSidebar(); setExpandedMoons([false, false, false, false, false, false, false, false]); }}>
         {isOpen ? 'Close' : 'Open'} Menu
       </div>
     </div>
