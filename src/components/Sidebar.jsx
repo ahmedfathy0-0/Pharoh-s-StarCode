c1e2840fcd4bfcf8822906acd4887db03351ed39
Ammar Yasser <143096817+AMYasserF@users.noreply.github.com>
2024-10-04 20:45:59 +0300

    resolve confilcts

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Sidebar.css';
 
-const Sidebar = ({ isOpen, toggleSidebar ,onPlanetClick }) => {
+const Sidebar = ({ isOpen, toggleSidebar, onPlanetClick }) => {
+  // State to track open/close status of moons list for each planet
+  const [expandedMoons, setExpandedMoons] = useState({});
+
+  // Toggle moons visibility for the clicked planet
+  const toggleMoons = (planet) => {
+    setExpandedMoons((prevState) => ({
+      ...prevState,
+      [planet]: !prevState[planet] // Toggle the state for the clicked planet
+    }));
+  };
+
   return (
     <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-      <h2>Navigation</h2>
+      <h2>NAVIGATION</h2>
       <ul className="planets">
       <li className='Mercury' onClick={() => onPlanetClick(1)}><a href="#">Mercury</a></li>
       <li className='Venus' onClick={() => onPlanetClick(2)}><a href="#">Venus</a></li>
@@ -32,8 +43,12 @@ const Sidebar = ({ isOpen, toggleSidebar ,onPlanetClick }) => {
             <li className='Titan' onClick={(e) => { e.stopPropagation(); onPlanetClick(16); }}><a href='#'>Titan</a></li>
             </ul>
         </li>
-        <li className='Uranus' onClick={() => onPlanetClick(7)}><a href="#">Uranus</a></li>
-        <li className='Neptune' onClick={() => onPlanetClick(8)}><a href="#">Neptune</a></li>
+        <li className='Uranus' onClick={() => onPlanetClick(7)}>
+          <a href="#">Uranus</a>
+        </li>
+        <li className='Neptune' onClick={() => onPlanetClick(8)}>
+          <a href="#">Neptune</a>
+        </li>
       </ul>
 
       <div className="sidebar-button" onClick={toggleSidebar}>
